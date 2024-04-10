50b3f70d6b0f1eb858038f1a31e6fee93b81f9ab
acederys <acederys@mail.ru>
2024-04-10 16:36:08 +0500

    Исправлен стиль кода для pep8

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
+"""Модель анализа текста."""
+
 from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
 
 class Item(BaseModel):
+    """Опредлеяем тип для класса."""
     text: str
-
 
 
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
 
-
 @app.get("/")
 def root():
+    """Основная страница с выводом сообщения."""
+
     return {"message": "Hello World"}
 
 
 @app.post("/predict/")
 def predict(item: Item):
+    """Вторая страница с подключенной моделью."""
+
     return classifier(item.text)[0]
--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+"""Тесты."""
+
 from fastapi.testclient import TestClient
 from main import app
 
@@ -5,12 +7,16 @@ client = TestClient(app)
 
 
 def test_read_main():
+    """Код ответа страницы."""
+
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "World"}
 
 
 def test_predict_positive():
+    """Код ответа и ответ модели положительного сообщения."""
+
     response = client.post("/predict/",
                            json={"text": "I like machine learning!"})
     json_data = response.json()
@@ -19,6 +25,8 @@ def test_predict_positive():
 
 
 def test_predict_negative():
+    """Код ответа и ответ модели отрицательного сообщения."""
+
     response = client.post("/predict/",
                            json={"text": "I hate machine learning!"})
     json_data = response.json()
