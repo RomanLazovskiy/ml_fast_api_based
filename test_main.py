6c1925d9a5372507429d6a23322be1fcda220be8
Roflanlico <92446877+Roflanlico@users.noreply.github.com>
2024-05-22 21:51:55 +0500

    Update test_main.py
    Добавил тесты, комбинируя положительную и отрицательную тональность в тексте.

--- test_main.py
+++ test_main.py
@@ -33,3 +33,20 @@ def test_predict_positive_negative():
     assert response.status_code == 200
     assert json_data['label'] == 'POSITIVE'
     
+
+def test_predict_negative_a_lot():
+    response = client.post('/predict/',
+                           json={'text':
+                                 'I love you, but I hate you,but I hate you, but I hate you, but I hate you,but I hate you, but I hate you'
+                                })
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['label'] == 'NEGATIVE'
+
+
+def test_predict_5050_firs_negative():
+    response = client.post('/predict/',
+                           json={'text': 'I hate you, but I love you'})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['label'] == 'POSITIVE'
