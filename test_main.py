bd07b78bad5a9e59defb14e50427797a304f6eeb
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2024-05-25 20:51:03 +0500

    Revert "add some tests"
    This reverts commit adda959dff49d71e910f64cdb343cde03fad41a8.

--- test_main.py
+++ test_main.py
@@ -24,21 +24,3 @@ def test_predict_negative():
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
-
-
-def test_predict_empty_text():
-    response = client.post("/predict/", json={"text": ""})
-    assert response.status_code == 200
-    result = response.json()
-    # Предполагается, что пустой текст может возвращать определённый результат, это можно изменить по необходимости
-    assert result["label"] in ["POSITIVE", "NEGATIVE"]
-    assert result["score"] > 0.5
-
-
-def test_predict_long_text():
-    long_text = "This is a very long text. " * 100  # Генерируем длинный текст
-    response = client.post("/predict/", json={"text": long_text})
-    assert response.status_code == 200
-    result = response.json()
-    assert result["label"] in ["POSITIVE", "NEGATIVE"]
-    assert result["score"] > 0.5
