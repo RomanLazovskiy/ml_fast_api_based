5b5c0bb431279c5238172b26d16dc2819172bd92
Александр Соломеин <alex@callibri.ru>
2024-05-25 22:47:41 +0500

    oops

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 
 def test_read_main():
     response = client.get("/")
-    assert response.status_code == 300
+    assert response.status_code == 200
     assert response.json() == {"message": "World"}
 
 
