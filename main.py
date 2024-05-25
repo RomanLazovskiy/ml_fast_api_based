f23422071f946d761474b28d54b0ccde599a1b36
vanek021 <vanekmenyakin@gmail.com>
2024-05-26 02:21:04 +0500

    Add documentation to Fast API application

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
 
 class Item(BaseModel):
-    text: str
+    text: str = Body(
+        embed=True,
+        title="Text",
+        description="""Text for analysis""",
+    )
 
 
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
 
-@app.get("/")
+@app.get("/", include_in_schema=False)
 def root():
     return {"message": "Hello World"}
 
 
-@app.post("/predict/")
+@app.post(
+    "/predict/",
+    response_model=Item,
+    summary="Make sentiment analysis",
+    description="""Make sentiment analysis
+         using sentiment-analysis HuggingFace model""",
+)
 def predict(item: Item):
     return classifier(item.text)[0]
