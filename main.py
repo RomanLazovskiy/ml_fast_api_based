82d98e5ad0f01fd2021916ab123585ba574c72e9
Александр Епик <a.epik@mail.ru>
2024-05-25 11:19:58 +0500

    format with black formatter

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from pydantic import BaseModel
 class Item(BaseModel):
     text: str
 
+
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
 # Переменные для хранения количества положительных и отрицательных отзывов
 positive_reviews_count = 0
 negative_reviews_count = 0
+
 
 @app.get("/")
 def root():
@@ -23,12 +25,13 @@ def predict(item: Item):
     global positive_reviews_count, negative_reviews_count
 
     prediction = classifier(item.text)[0]
-    if prediction['label'] == 'POSITIVE':
+    if prediction["label"] == "POSITIVE":
         positive_reviews_count += 1
-    elif prediction['label'] == 'NEGATIVE':
+    elif prediction["label"] == "NEGATIVE":
         negative_reviews_count += 1
-    
+
     return prediction
+
 
 @app.get("/analytics")
 def get_analytics():
@@ -38,8 +41,18 @@ def get_analytics():
         "total_reviews": positive_reviews_count + negative_reviews_count,
         "positive_reviews": positive_reviews_count,
         "negative_reviews": negative_reviews_count,
-        "positive_percentage": (positive_reviews_count / (positive_reviews_count + negative_reviews_count)) * 100 if positive_reviews_count + negative_reviews_count > 0 else 0,
-        "negative_percentage": (negative_reviews_count / (positive_reviews_count + negative_reviews_count)) * 100 if positive_reviews_count + negative_reviews_count > 0 else 0
+        "positive_percentage": (
+            (positive_reviews_count / (positive_reviews_count + negative_reviews_count))
+            * 100
+            if positive_reviews_count + negative_reviews_count > 0
+            else 0
+        ),
+        "negative_percentage": (
+            (negative_reviews_count / (positive_reviews_count + negative_reviews_count))
+            * 100
+            if positive_reviews_count + negative_reviews_count > 0
+            else 0
+        ),
     }
 
     return analytics_data
