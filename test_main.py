b53baf28843cfb7650b4f1a47b5d92bb1b211d8a
Alexandr <118203209+BandrD@users.noreply.github.com>
2024-05-26 17:30:23 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,24 +3,27 @@ from main import app
 
 client = TestClient(app)
 
-
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "World"}
-
+    assert response.json() == {"message": "Hello World"}
 
 def test_predict_positive():
-    response = client.post("/predict/",
-                           json={"text": "I like machine learning!"})
+    response = client.post("/predict/", json={"text": "I like machine learning!"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'POSITIVE'
 
-
 def test_predict_negative():
-    response = client.post("/predict/",
-                           json={"text": "I hate machine learning!"})
+    response = client.post("/predict/", json={"text": "I hate machine learning!"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
+
+def test_predict_proba():
+    response = client.post("/predict_proba/", json={"text": "I love programming!"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert 'label' in json_data
+    assert 'score' in json_data
+    assert 'details' in json_data
