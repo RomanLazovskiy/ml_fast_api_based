6e0bcb0209d1a1aa02b04f96a95009e4fa570033
VasyaRodionov1 <vasyarod@gmail.com>
2024-05-26 22:27:32 +0500

    close errors v4

--- test_main.py
+++ test_main.py
@@ -1,66 +1,66 @@
 from fastapi.testclient import TestClient
 from main import app
 
-cl = TestClient(app)
+c = TestClient(app)
 
 
 def test_read_main():
-    r = cl.get("/")
+    r = c.get("/")
     assert r.status_code == 200
     assert r.json() == {"message": "World"}
 
 
 def test_predict_positive():
-    r = cl.post("/predict/", json={"text": "I like machine learning!"})
+    r = c.post("/predict/", json={"text": "I like machine learning!"})
     json_data = r.json()
     assert r.status_code == 200
     assert json_data["label"] == "POSITIVE"
 
 
 def test_predict_negative():
-    r = cl.post("/predict/", json={"text": "I hate machine learning!"})
+    r = c.post("/predict/", json={"text": "I hate machine learning!"})
     json_data = r.json()
     assert r.status_code == 200
     assert json_data["label"] == "NEGATIVE"
 
 
 def test_predict_positive_weather():
-    r = cl.post("/predict/", json={"text": "The weather is beautiful today!"})
+    r = c.post("/predict/", json={"text": "The weather is beautiful today!"})
     json_data = r.json()
     assert r.status_code == 200
     assert json_data["label"] == "POSITIVE"
 
 
 def test_predict_negative_weather():
-    r = cl.post("/predict/", json={"text": "The weather is terrible!"})
+    r = c.post("/predict/", json={"text": "The weather is terrible!"})
     json_data = r.json()
     assert r.status_code == 200
     assert json_data["label"] == "NEGATIVE"
 
 
 def test_predict_positive_food():
-    r = cl.post("/predict/", json={"text": "This pizza tastes amazing!"})
+    r = c.post("/predict/", json={"text": "This pizza tastes amazing!"})
     json_data = r.json()
     assert r.status_code == 200
     assert json_data["label"] == "POSITIVE"
 
 
 def test_predict_negative_food():
-    r = cl.post("/predict/", json={"text": "This pizza tastes awful!"})
+    r = c.post("/predict/", json={"text": "This pizza tastes awful!"})
     json_data = r.json()
     assert r.status_code == 200
     assert json_data["label"] == "NEGATIVE"
 
 
 def test_predict_positive_weather_conditional():
-    r = cl.post("/predict/", json={"text": "The weather is nice today."})
+    r = c.post("/predict/", json={"text": "The weather is nice today."})
     json_data = r.json()
     assert r.status_code == 200
     assert json_data["label"] == "POSITIVE"
 
 
 def test_predict_negative_weather_sarcasm():
-    r = cl.post("/predict/", json={"text": "Oh, another rainy day ..."})
+    r = c.post("/predict/", json={"text": "Oh, another rainy day ..."})
     json_data = r.json()
     assert r.status_code == 200
     assert json_data["label"] == "NEGATIVE"
