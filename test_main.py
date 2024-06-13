4cc80dc2a11db1179c48693a8ebf284a26455097
Alexandra Khusainova <khusainova.alexandra.20@gmail.com>
2024-06-13 18:26:27 +0500

    Fix test

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from main import app
+from api import app
 
 client = TestClient(app)
 
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "World"}
+    assert response.json() == {"message": "Hello World"}
 
 
 def test_predict_positive():
